97247c2f20ef85511b28d8d46d17c235fee102ab
Georg Jung <gjung@gimas.net>
2026-03-06 17:51:05 +0100

    set Console.OutputEncoding = utf8

--- src/UpdateCpmVersions/Program.cs
+++ src/UpdateCpmVersions/Program.cs
@@ -2,6 +2,10 @@ using System.CommandLine;
 
 using UpdateCpmVersions;
 
+// dotnet CLI does this too
+// https://github.com/dotnet/sdk/blob/5bb7e315bf797af120abf7ed17b7cbd6145b48f6/src/Cli/dotnet/Program.cs#L48
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
 Argument<string?> pathArg = new("path")
 {
     Description = "Path to Directory.Packages.props or a directory containing it",
